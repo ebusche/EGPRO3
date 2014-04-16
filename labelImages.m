de8c69b31542858446c103728706ad50f2196265
giangdoan <gianghdoan@gmail.com>
2014-04-16 12:03:45 -0500

    update labelImage

--- labelImages.m
+++ labelImages.m
@@ -1,13 +1,9 @@
 function [categories] = labelImages( folder , newfolder)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-
 files = dir([folder, '/*']);
 mkdir(newfolder);
 
-categories = cell(5); %cell(size(files));
-for i = 1: 5 %size(files)
+categories = cell(size(files));
+for i = 1: size(files)
     
     if(~strcmp(files(i).name, '.') && ~strcmp(files(i).name, '..'))
         
@@ -17,8 +13,7 @@ for i = 1: 5 %size(files)
         categories{i} = suffix;
        
         for j = 1: size(images);
-           % files(i).name
-            %images(j).name
+          
             orgImage = [folder '/' files(i).name '/' images(j).name];
             newImage = [newfolder '/' images(j).name(1:end-4) suffix '.jpg'];
             copyfile(orgImage,newImage);
